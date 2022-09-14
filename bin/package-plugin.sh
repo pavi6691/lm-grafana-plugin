8ebd496b6402b94963e578e43b7fc886cd704340
praveen.karadi@logicmonitor.com <praveen.karadi@logicmonitor.com>
2022-09-14 11:44:17 +0530

    package-and:  avoid pugincheck for now

--- bin/package-plugin.sh
+++ bin/package-plugin.sh
@@ -33,6 +33,6 @@ cp CHANGELOG.md ci/jobs/build_plugin/dist
 
 # Package and sign plugin
 echo -e "${GREEN}Packaging and signing plugin${NC}"
-yarn grafana-toolkit plugin:ci-package --rootUrls http://localhost:3000
+yarn grafana-toolkit plugin:ci-package
 
 echo -e "${GREEN}Packaged plugin located in ${BLUE}ci/packages${NC}"
